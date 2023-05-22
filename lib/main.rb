0e6bfa7962d8b10523bc816821413a2a5bf3ff27
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-22 16:34:19 +0200

    feat: add Node class
    Added the Node class, it will be used to construct a binary search
    tree, hence the left_node and right_node attributes. It also includes
    the comparable module, which will allow for easier comparison between
    the data of two different nodes, to determine where the node will go
    in the BST.

---
+++ lib/main.rb
@@ -0,0 +1,15 @@
+class Node
+  attr_accessor :data, :left_node, :right_node
+
+  include Comparable
+
+  def initialize(data)
+    @data = data
+    @left_node = nil
+    @right_node = nil
+  end
+
+  def <=>(other_node)
+    self.data <=> other_node.data
+  end
+end
