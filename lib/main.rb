97960f93bf994335e75b5b1bde44aa8d9aeae07a
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-22 17:23:34 +0200

    feat: add pretty_print method
    Added the pretty_print method to visualize binary tree.

--- lib/main.rb
+++ lib/main.rb
@@ -34,4 +34,10 @@ class Tree
 
     return node
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
+  end
 end
