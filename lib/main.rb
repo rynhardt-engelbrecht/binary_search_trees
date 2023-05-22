d26320bad5b685ac25752a7a9590ea2ac18cb5e7
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-22 17:12:15 +0200

    feat: add build_tree instance method
    Added the build_tree instance method to construct a binary search tree
    from an array (passed as argument).

--- lib/main.rb
+++ lib/main.rb
@@ -9,11 +9,29 @@ class Node
     @right_node = nil
   end
 
-  def <=>(other_node)
-    self.data <=> other_node.data
-  end
+  # def <=>(other_node)
+  #   self.data <=> other_node.data
+  # end
 end
 
 class Tree
   attr_accessor :root
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    if root == nil
+      array.uniq!
+      array.sort!
+    end
+
+    middle = array.length/2
+    node = Node.new(array[middle])
+    self.root = node if root == nil
+
+    node.left_node = build_tree(array.slice(0, middle))
+    node.right_node = build_tree(array.slice(middle + 1, array.length))
+
+    return node
+  end
 end
